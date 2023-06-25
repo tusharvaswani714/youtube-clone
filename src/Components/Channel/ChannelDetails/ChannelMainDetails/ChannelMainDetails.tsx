34a0bb976ecb4cd3ad7d91ca395697d959867b91
tusharvaswanicoder <codewithtusharvaswani@gmail.com>
2023-06-25 18:26:37 +0530

    Made channel page responsive

--- src/Components/Channel/ChannelDetails/ChannelMainDetails/ChannelMainDetails.tsx
+++ src/Components/Channel/ChannelDetails/ChannelMainDetails/ChannelMainDetails.tsx
@@ -7,18 +7,18 @@ const ChannelMainDetails = () => {
     );
     if (!channelDetails) return null;
     return (
-        <div className="flex gap-[2.4rem] items-center px-20 py-8">
+        <div className="flex flex-col sm:flex-row gap-[1.6rem] sm:gap-[2.4rem] items-center px-10 py-6 sm:px-20 sm:py-8 text-center sm:text-left">
             <div
-                className="flex-shrink-0 bg-cover bg-center w-52 h-52 rounded-full"
+                className="flex-shrink-0 bg-cover bg-center w-32 h-32 sm:w-52 sm:h-52 rounded-full"
                 style={{
                     backgroundImage: `url(${channelDetails.profileURL})`,
                 }}
             />
-            <div>
-                <div className="text-primary-light-900 font-normal text-[2.4rem]">
+            <div className="overflow-hidden">
+                <div className="text-primary-light-900 font-normal text-[1.8rem] sm:text-[2.4rem]">
                     {channelDetails.name}
                 </div>
-                <div className="text-primary-light-800 text-[1.4rem] flex flex-col gap-4">
+                <div className="text-primary-light-800 text-[1.4rem] flex items-center sm:items-start flex-col gap-2 sm:gap-4">
                     <div className="flex gap-4">
                         <span>
                             {millify(channelDetails.subscribers, {
@@ -33,7 +33,7 @@ const ChannelMainDetails = () => {
                             videos
                         </span>
                     </div>
-                    <div className="max-w-3xl text-ellipsis overflow-hidden whitespace-nowrap">
+                    <div className="max-w-md sm:max-w-3xl text-ellipsis overflow-hidden whitespace-nowrap">
                         {channelDetails.desc}
                     </div>
                 </div>
